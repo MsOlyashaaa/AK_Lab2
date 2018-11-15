7ad23011a24d5ec3939bc3d16df6ef7f9cb13a60
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    add a multiplication operation Petrenko
    # Conflicts:
    #	calculator.h

--- calculator.cpp
+++ calculator.cpp
@@ -1,5 +1,17 @@
 #include "calculator.h"
 
+//Petrenko Olha
+//1 line
+//2 line
+//3 line
+//4 line
+//5 line
+//6 line
+//7 line
+//8 line
+//9 line
+//10 line
+
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
@@ -8,4 +20,9 @@ int Calculator::Add (double a, double b)
 int Calculator::Sub (double a, double b)
 {
 	return Add (a, -b);
+}
+
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
 }
